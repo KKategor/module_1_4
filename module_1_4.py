c26190e31750e3e9b42b8b22e59e5a94ab7f9da7
KKategor <kamchatkasweethome@gmail.com>
2024-09-30 20:46:00 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+# Lesson string methods
+
+
+my_string =input('Укажите Ваше любимое блюдо: ')
+l = len(my_string)
+print(l)
+print(my_string . upper())
+print(my_string . lower())
+print(my_string . replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
